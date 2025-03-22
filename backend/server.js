f31c8da3ea3c95a39fde27ae763110e0c2f27d07
Souryadip Mallick <souryadipmallick118@gmail.com>
2025-03-22 13:12:22 +0530

    Fixed minor bug

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
 const userRoutes = require('./routes/userRoutes');
@@ -8,22 +7,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
-// Serve frontend production build
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
 // Add routes
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);   // Mount the user routes
 
 app.get('/', (req, res) => {
   res.send('Lost and Found API Running...');
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
