2e736e15f93fa904b590f35969e6853b8f748636
Souryadip Mallick <souryadipmallick118@gmail.com>
2025-03-22 13:22:58 +0530

    Updated backend to serve frontend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
@@ -11,8 +12,18 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-// Add routes
-app.use('/api/users', userRoutes);   // Mount the user routes
+// Serve static files from the frontend's build directory
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  
+  // All other routes should serve the React app (catch-all route)
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+// API routes
+app.use('/api/users', userRoutes); 
 
 app.get('/', (req, res) => {
   res.send('Lost and Found API Running...');
