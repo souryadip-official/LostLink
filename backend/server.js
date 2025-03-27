875672c01226f1bc070c1402e33784e4e08fe3c2
Souryadip Mallick <souryadipmallick118@gmail.com>
2025-03-27 23:49:13 +0530

    Fixed Server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // Middleware to verify JWT tokens
 const verifyToken = (req, res, next) => {
